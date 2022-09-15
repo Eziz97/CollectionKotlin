f1520d6f6f5c50f33960052f4a826766fda1afce
Eziz97 <ezizdowletowic@gmail.com>
2022-09-16 00:13:14 +0300

    add HastSet

--- src/main/kotlin/Collection/Map.kt
+++ src/main/kotlin/Collection/Map.kt
@@ -1,7 +1,23 @@
 package Collection
 
 fun main() {
+    //не изменяемая коллекция
     var mapList = mapOf<Int?,String>(null to "Rostov" , 200 to "Mosscow")
     println(mapList)
+
+
+    //изменяемая коллекция
+    var mapList2 = mutableMapOf<Int,String>(222 to "SP" , 333 to "vladiwastok")
+    mapList2.set(333,"Mary")
+    println(mapList2)
+    var mapList3 = HashMap<Int ,String>(mapList2)
+    mapList3.put(666,"Ashgabat")
+    mapList3.remove(222)
+    mapList3.put(666,"balkan")
+    for (mutableEntry in mapList3) {
+        println(" ${mutableEntry.key} ${mutableEntry.value}" )
+
+
+    }
 
 }
